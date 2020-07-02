dcd195c424fc3c66ebcc4d5a854439e169d76c82
Dmitry Semenchuk <dmitry.semenchuk@dataart.com>
2020-07-02 18:03:41 +0300

    refactor

--- commands/commands.go
+++ commands/commands.go
@@ -19,34 +19,36 @@ var Newenv = func(c *cli.Context) error {
 	execShell("make clean-env", nil)
 	execShell(fmt.Sprintf("BUILD=%s make localize-standalone-only", build), nil)
 
-	if !c.Bool("noup") {
-		defaultUp := []string{"core", "dhcp", "data", "dns"}
-		execShell(fmt.Sprintf("docker-compose up -d %s", strings.Join(defaultUp, " ")), nil) // TODO: configurable params
-
-		if c.Bool("nobs") {
-			return nil
-		}
-
-		var watchCloser termCloser
-		go func() {
-			i := 200
-			for i > 0 {
-				res := execAndReturn("docker ps | grep -c '(healthy)'")
-				if strings.TrimSpace(res) == strconv.Itoa(len(defaultUp)) {
-					watchCloser.Close()
-					return
-				}
-				time.Sleep(1 * time.Second)
-				i--
-			}
-			watchCloser.Close()
-		}()
-
-		execShell(`watch docker ps --format \"table {{.ID}}\\t{{.Names}}\\t{{.Status}}\"`, &watchCloser)
-		execShell("clear", nil)
-
-		execShell("make bootstrap-api", nil)
+	if c.Bool("noup") {
+		return nil
 	}
+
+	defaultUp := []string{"core", "dhcp", "data", "dns"}
+	execShell(fmt.Sprintf("docker-compose up -d %s", strings.Join(defaultUp, " ")), nil) // TODO: configurable params
+
+	if c.Bool("nobs") {
+		return nil
+	}
+
+	var watchCloser termCloser
+	go func() {
+		i := 200
+		for i > 0 {
+			res := execAndReturn("docker ps | grep -c '(healthy)'")
+			if strings.TrimSpace(res) == strconv.Itoa(len(defaultUp)) {
+				watchCloser.Close()
+				return
+			}
+			time.Sleep(1 * time.Second)
+			i--
+		}
+		watchCloser.Close()
+	}()
+
+	execShell(`watch docker ps --format \"table {{.ID}}\\t{{.Names}}\\t{{.Status}}\"`, &watchCloser)
+	execShell("clear", nil)
+
+	execShell("make bootstrap-api", nil)
 
 	return nil
 }
